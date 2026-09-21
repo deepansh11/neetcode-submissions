4e2447969ea4b44a72ac8d39249d9e9075123945
deepansh11 <34342846+deepansh11@users.noreply.github.com>
2026-09-21 16:30:54 +0530

    Add: find-the-town-judge - submission-1

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        delta = defaultdict(int)
+        for a,b in trust:
+            delta[a] -= 1 # a trusts someone, can't be a judge
+            delta[b] += 1 # b gains one trust
+        for i in range(1,n+1):
+            if delta[i] == n-1:
+                return i
+        return -1
